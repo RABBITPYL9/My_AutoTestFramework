f7f4681ce690a2785d5f06ea01c8af4f9a27fd37
RABBITPYL9 <shubins317@gmail.com>
2021-05-15 19:54:29 +0300

    Оптимизация кода, добавлены комментарии

--- test_cardexclusion_case1.py
+++ test_cardexclusion_case1.py
@@ -33,17 +33,13 @@ NSMAP = {
     'mux': "http://www.psit.ru/3CardF/MUXAntiFraud"
 }
 
-class CardExclusionAddTest(unittest.TestCase):#получение и проверка даты начала,конца отпуска(исключения), страны отпуска
+class CardExclusionAddTest(unittest.TestCase):
     def test_send_xml_body_from_string_check_status_code_and_content_type(self):
         response = requests.post("http://192.168.131.158:8088",headers={"Content-Type": "text/xml"},data=fixed_xml_body_as_string())
+        response_body_as_xml = et.fromstring(response.content)
+        xml_tree = et.ElementTree(response_body_as_xml)
         answer = response.content
         response_body_as = et.fromstring(answer)
         xml_tr = et.ElementTree(response_body_as)
-        status_code = response.status_code
-        assert status_code == 200
-        structure_startdate = xml_tr.find('*//mux:StartDateTime', namespaces=NSMAP)
-        assert structure_startdate.text == '2021-05-12T00:00:00'
-        structure_enddate = xml_tr.find('*//mux:EndDateTime', namespaces=NSMAP)
-        assert structure_enddate.text == '2021-05-20T00:00:00'
-        structure_country = xml_tr.find('*//mux:Country', namespaces=NSMAP)
-        assert structure_country.text == 'ABW'
+        savings_accounts = xml_tr.find('*//mux:Country', namespaces=NSMAP)
+        assert savings_accounts.text == 'ABW'
